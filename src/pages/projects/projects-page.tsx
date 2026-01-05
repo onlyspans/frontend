c1dc8690e697da85927e0237c7aa4b60c9e057de
milkylake <ajnaliev07@gmail.com>
2026-01-05 14:25:29 +0300

    fix container

--- src/pages/projects/projects-page.tsx
+++ src/pages/projects/projects-page.tsx
@@ -30,7 +30,7 @@ export function ProjectsPage() {
   };
 
   return (
-    <div className="container space-y-6">
+    <div className="space-y-6">
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-3xl font-bold">Projects</h1>
