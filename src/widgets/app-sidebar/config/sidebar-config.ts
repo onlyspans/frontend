900272c873e4fce7ea66100ce51053e4bed030a9
milkylake <ajnaliev07@gmail.com>
2026-03-26 11:22:22 +0300

    update sidebar-config.ts

--- src/widgets/app-sidebar/config/sidebar-config.ts
+++ src/widgets/app-sidebar/config/sidebar-config.ts
@@ -126,8 +126,6 @@ export const sidebarConfig: SidebarConfig = {
       items: [
         { title: 'sidebar.navbar.allEnvironments', url: '/environments' },
         { title: 'sidebar.navbar.variables', url: '/environments/variables' },
-        { title: 'sidebar.navbar.kubernetes', url: '/environments/kubernetes' },
-        { title: 'sidebar.navbar.virtualMachines', url: '/environments/vms' }
       ]
     },
     {
